1425d97944f7872d342e260951ce4fd3d55df460
alinabokaya <ali.user.inc@gmail.com>
2022-07-28 22:56:44 +0200

    1. Added new tests for new pages 2. Renamed classes names

--- HerokuApp/src/test/java/constants/Urls.java
+++ HerokuApp/src/test/java/constants/Urls.java
@@ -3,4 +3,8 @@ package constants;
 public class Urls {
     public static final String ADD_REMOVE_PAGE = "http://the-internet.herokuapp.com/add_remove_elements/";
     public static final String DATA_TABLES_PAGE = "http://the-internet.herokuapp.com/tables";
+    public static final String CHECKBOXES_PAGE = "http://the-internet.herokuapp.com/checkboxes";
+    public static final String INPUTS_PAGE = "http://the-internet.herokuapp.com/inputs";
+    public static final String TYPOS_PAGE = "http://the-internet.herokuapp.com/typos";
+    public static final String NOTIFICATION_MESSAGE_PAGE = "http://the-internet.herokuapp.com/notification_message_rendered";
 }
